374c32e9451fa517c2de1e782d18e8910a09c254
gabriel marques <gabrielmarksf@icloud.com>
2023-08-18 11:25:11 -0300

    criação json e splash page

---
+++ android/app/src/main/kotlin/br/com/academiadogabe/dw_barbershop/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.academiadogabe.dw_barbershop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/barbershop_app.dart
@@ -0,0 +1,25 @@
+import 'package:asyncstate/widget/async_state_builder.dart';
+import 'package:dw_barbershop/src/core/ui/widgets/barbershop_loader.dart';
+import 'package:flutter/material.dart';
+
+import 'features/splash/splash_page.dart';
+
+class BarbershopApp extends StatelessWidget {
+  const BarbershopApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AsyncStateBuilder(
+      customLoader: const BarbershopLoader(),
+      builder: (asyncNavigatorObserver) {
+        return MaterialApp(
+          title: 'DW Barbershop',
+          navigatorObservers: [asyncNavigatorObserver],
+          routes: {
+            '/': (_) => const SplashPage(),
+          },
+        );
+      },
+    );
+  }
+}
