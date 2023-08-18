95eba5b852357b4e8d3e1d294b975b9f9ada81a1
gabriel marques <gabrielmarksf@icloud.com>
2023-08-18 16:36:59 -0300

    atualização splash page e padronização do tema

--- lib/src/barbershop_app.dart
+++ lib/src/barbershop_app.dart
@@ -1,5 +1,7 @@
 import 'package:asyncstate/widget/async_state_builder.dart';
+import 'package:dw_barbershop/src/core/ui/barbershop_theme.dart';
 import 'package:dw_barbershop/src/core/ui/widgets/barbershop_loader.dart';
+import 'package:dw_barbershop/src/features/auth/login/login_page.dart';
 import 'package:flutter/material.dart';
 
 import 'features/splash/splash_page.dart';
@@ -13,10 +15,12 @@ class BarbershopApp extends StatelessWidget {
       customLoader: const BarbershopLoader(),
       builder: (asyncNavigatorObserver) {
         return MaterialApp(
+          theme: BarbershopTheme.themeData,
           title: 'DW Barbershop',
           navigatorObservers: [asyncNavigatorObserver],
           routes: {
             '/': (_) => const SplashPage(),
+            '/auth/login': (_) => const LoginPage(),
           },
         );
       },
---
+++ lib/src/core/ui/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+sealed class ColorsConstants {
+  static const brown = Color(0XFFB07B01);
+  static const grey = Color(0XFF999999);
+  static const greyLight = Color(0XFFE6E2E9);
+  static const red = Color(0XFFEB1212);
+}
